05b0449f16b988cdcee1567e04f91e40f3c8e234
Amber Forrester <DivineIntelligence3.0@gmail.com>
2024-07-23 23:00:15 -0400

    Redux toolkit installed

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+// Create some initial values. 
+const initialState = {
+    currentUser: null,
+    error: null, 
+    loading: false
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: { // Reducers have special functions that perform actions = sign in start, sign in success, or sign in failure. 
+        SignInStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+
+        SignInSuccess: (state, action) => { // Storing the payload with the information we need to store in the state. 
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+
+        signInFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+
+        
+    }
+});
+
+
+
+export default userSlice.reducer;
