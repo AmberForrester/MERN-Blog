dd7278507d043953361b6a2003a605c1aff16b7a
Amber Forrester <DivineIntelligence3.0@gmail.com>
2024-07-28 19:36:43 -0400

    Dynamic Post Page w/integration - DONE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 
 
 const App = () => {
@@ -39,6 +40,7 @@ const App = () => {
 
 
         <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/post/:postSlug' element={<PostPage/>}/>
 
 
 
