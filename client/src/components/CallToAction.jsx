21771ae5b8fb84af3150e7d4475e85ec2d633231
Amber Forrester <DivineIntelligence3.0@gmail.com>
2024-07-29 01:26:10 -0400

    Call To Action UI - DONE

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -2,20 +2,19 @@ import { Button } from "flowbite-react";
 
 const CallToAction = () => {
   return (
-    <div className="flex flex-col sm:flex-row p-3 border  justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+    <div className="flex flex-col sm:flex-row p-3 border justify-center items-center rounded-tl-3xl rounded-br-3xl text-center bg-white dark:bg-gray-800">
       <div className="flex-1 justify-center flex flex-col">
         <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
         <p className="text-gray-500 my-2">Checkout these resources with 100 JavaScript Projects</p>
 
         <Button
-          className='bg-gradient-to-r from-cyan-500 via-blue-500 to-indigo-700 rounded-lg text-white no-outline' outline>
-          <a
-            href="https://www.100jsprojects.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            100 JavaScript Projects
-          </a>
+          className='bg-gradient-to-r from-cyan-500 via-blue-500 to-indigo-700 rounded-lg text-white no-outline dark:bg-gradient-to-r dark:from-indigo-700 dark:via-blue-500 dark:to-cyan-500'
+          href="https://www.100jsprojects.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          outline
+        >
+          100 JavaScript Projects
         </Button>
       </div>
       <div className="p-7 flex-1">
