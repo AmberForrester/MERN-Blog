4d55aeb394642dfa15ec014efe0b8c81c982cea6
Amber Forrester <DivineIntelligence3.0@gmail.com>
2024-07-29 01:53:38 -0400

    Header dropdown menu updated for dashboard link + functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -121,6 +121,11 @@ const Header = () => {
                             <span className='block text-sm'>@{currentUser.username}</span>
                             <span className="block text-sm font-medium truncate">{currentUser.email}</span>  
                           </Dropdown.Header>
+
+                          <Link to={'/dashboard?tab=dash'}>
+                            <Dropdown.Item>Dashboard</Dropdown.Item>
+                          </Link>
+                          <Dropdown.Divider/>
                           
                           <Link to={'/dashboard?tab=profile'}>
                             <Dropdown.Item>Profile</Dropdown.Item>
